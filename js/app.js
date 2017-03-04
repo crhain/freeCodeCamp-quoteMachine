b91192dfdeab8cb82a45a2c4b1c00e3b631dbca3
Carl Hain <crhain@gmail.com>
2017-03-04 07:18:51 -0600

    Clean code to improve namespacing and make things easier to read

--- js/app.js
+++ js/app.js
@@ -1,70 +1,77 @@
-var currentFormatedQuote = '';
-var currentQuote = '';
-var currentAuthor = '';
 
-function getQuote(){
-//Set up text of quote so it starts hidden and indicates it is loading
-$(".hidden").css("opacity", 0.0);
-$("#quote-text").html('Loading...');
-$("#quote-byline").html('');
+var quote = (function(){
+  var module = {};
+  var currentFormatedQuote = '';
+  var currentQuote = '';
+  var currentAuthor = '';
 
-//Set up some variables for ajax request and the ajax request itself
-var url = 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=';
+  function getQuote(){
+    //Set up text of quote so it starts hidden and indicates it is loading
+    $(".hidden").css("opacity", 0.0);
+    $("#quote-text").html('Loading...');
+    $("#quote-byline").html('');
 
-var key='OcI6bew9zhmshl2EtCEA84x2sF4Lp1C53f5jsn5JSaSDsd7w2u';
-var request = $.ajax({
-headers: {
-'X-Mashape-Key': key,
-'Accept': 'application/json',
-'Content-Type': 'application/x-www-form-urlencoded'
-},
-url: url,
-dataType: 'json'
-});
+    //Set up some variables for ajax request and the ajax request itself
+    var url = 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=';
 
-request.done(function( jsonText ) {
+    var key='OcI6bew9zhmshl2EtCEA84x2sF4Lp1C53f5jsn5JSaSDsd7w2u';
+    var request = $.ajax({
+      headers: {
+      'X-Mashape-Key': key,
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      url: url,
+      dataType: 'json'
+    });
 
-//Set fade effect for quote text
-$(".hidden").fadeTo(1000, 1);
+    request.done(function( jsonText ) {
 
-//set text
-$("#quote-text").html( '"' + jsonText.quote + '"');
-$("#quote-byline").html('- ' + jsonText.author);
+      //Set fade effect for quote text
+      $(".hidden").fadeTo(1000, 1);
 
-//set some variables to track current text
-currentQuote = '"' + jsonText.quote + '"';
-currentAuthor = jsonText.author;
-currentFormatedQuote = currentQuote + ' - ' + currentAuthor;
+      //set text
+      $("#quote-text").html( '"' + jsonText.quote + '"');
+      $("#quote-byline").html('- ' + jsonText.author);
 
-});
+      //set some variables to track current text
+      currentQuote = '"' + jsonText.quote + '"';
+      currentAuthor = jsonText.author;
+      currentFormatedQuote = currentQuote + ' - ' + currentAuthor;
+    });
 
-request.error(function( error ){
- var errorMessage = "Error! Cannot Load Resource.";
- alert(errorMessage);
-});
+    request.error(function( error ){
+     var errorMessage = "Error! Cannot Load Resource.";
+     alert(errorMessage);
+    });
 
-}
+  }//end of getQuote()
+
+  module.currentFormatedQuote = currentFormatedQuote;
+  module.currentQuote = currentQuote;
+  module.currentAuthor = currentAuthor;
+  module.getQuote = getQuote;
+
+  return module;
+
+})();
 
 //jquery main
 $("document").ready(function(){
-getQuote(); //get a quote when first initializing the page
+  quote.getQuote(); //get a quote when first initializing the page
 //handle quote button clicks
-$("#btn-quote").on("click", function(){
-getQuote();
-});
+  $("#btn-quote").on("click", function(){
+    quote.getQuote();
+  });
 
-//handle twitter button clicks
-$("#btn-twitter").on("click", function(){
-var twitterURL = "https://twitter.com/intent/tweet" + "?text=" + encodeURIComponent(currentFormatedQuote) + "&hashtags=quote";
-window.open(twitterURL, "Share");
+  //handle twitter button clicks
+  $("#btn-twitter").on("click", function(){
+    var twitterURL = "https://twitter.com/intent/tweet" + "?text=" + encodeURIComponent(quote.currentFormatedQuote) + "&hashtags=quote";
+    window.open(twitterURL, "Share");
+  });
+  //handle tumblr button clicks
+  $("#btn-tumblr").on("click", function(){
+    var tumblrURL = "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags='#quote'&quote=" + quote.currentQuote + "&caption=" + quote.currentAuthor;
+    window.open(tumblrURL, "Share");
+  });
 });
-
-//handle tumblr button clicks
-$("#btn-tumblr").on("click", function(){
-var tumblrURL = "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags='#quote'&quote=" + currentQuote + "&caption=" + currentAuthor;
-window.open(tumblrURL, "Share");
-});
-
-
-});
-//# sourceURL=pen.js
